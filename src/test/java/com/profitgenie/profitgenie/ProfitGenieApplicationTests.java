87477942b0b07c22b054d52a5f9e652cbd59fb22
Luke Garrigan <lukegarrigan8@gmail.com>
2018-09-08 13:46:05 +0100

    First commit - created new project as setup spring boot incorrectly

---
+++ src/test/java/com/profitgenie/profitgenie/ProfitGenieApplicationTests.java
@@ -0,0 +1,16 @@
+package com.profitgenie.profitgenie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProfitGenieApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
